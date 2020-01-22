7efd1eb8aeea66731ae9c3c7f56acb5cb9600fd2
Daniel Oberhoff <daniel@danieloberhoff.de>
2020-01-22 13:32:25 +0100

    use our own branch and proper include dir

--- conanfile.py
+++ conanfile.py
@@ -3,19 +3,19 @@ from conans import tools
 
 class ffnvcodecConan(ConanFile):
     name = "ffnvcodec"
-    version = "9.0.18.1"
-    description = "nv-codec-headers for ffmpeg https://github.com/FFmpeg/nv-codec-headers"
-    url = "https://github.com/omaralvarez/conan-ffnvcodec"
-    repo_url = "https://github.com/FFmpeg/nv-codec-headers"
+    version = "9.0.18.2"
+    description = "nv-codec-headers for ffmpeg https://github.com/maddanio/nv-codec-headers.git"
+    url = "https://github.com/pixelwise/conan-ffnvcodec"
+    repo_url = "https://github.com/maddanio/nv-codec-headers.git"
     topics = ("c", "header-only")
     generators = "cmake"
     no_copy_source = True
 
     def source(self):
-        self.run("git clone -b 'n%s' --single-branch --depth 1 %s" % (self.version, self.repo_url))
-    
+        self.run("git clone -b modules_merged --single-branch --depth 1 %s" % (self.repo_url))
+
     def package(self):
-        self.copy("*.h", dst="include", keep_path=False)
+        self.copy("*.h", dst="include/ffnvcodec", keep_path=False)
 
     def package_id(self):
         self.info.header_only()
